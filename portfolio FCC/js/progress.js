b37d5427e1680ba13d811e1e9855bc68316dd74f
Patrick Tłuszcz <patricktluszcz@gmail.com>
2016-05-31 20:59:51 +0200

    poprawka wciez i svp

--- portfolio FCC/js/progress.js
+++ portfolio FCC/js/progress.js
@@ -1,44 +1,45 @@
-window.onload = function (){
+window.onload = function () {
 
-    var envelope = document.getElementById('email-display'),
-        contactForm = document.getElementById('email-contact-form'),
-        main = document.getElementsByTagName('main')[0],
-        burger = document.getElementById('hamburger'),
-        menu = document.getElementById('header'),
-        exit = document.getElementById('exit-btn-contact'),
-        opacity = 0,
-        interval;
+	var envelope = document.getElementById('email-display');
+	var contactForm = document.getElementById('email-contact-form');
+	var main = document.getElementsByTagName('main')[0];
+	var burger = document.getElementById('hamburger');
+	var menu = document.getElementById('header');
+	var exit = document.getElementById('exit-btn-contact');
+	var opacity = 0;
+	var interval;
 
-        function fadeIn(){
-            opacity += 0.01;
-            contactForm.style.opacity = opacity;
-            if( opacity >= 1 ) clearInterval(interval);
-        }
-        function fadeOut(){
-            opacity -= 0.01;
-            contactForm.style.opacity = opacity;
-            if( opacity <= 0 ) {
-                contactForm.style.display = "none";
-                clearInterval(interval);
-        }
+	function fadeIn() {
+		opacity += 0.01;
+		contactForm.style.opacity = opacity;
+		if (opacity >= 1) clearInterval(interval);
+	}
+
+	function fadeOut() {
+		opacity -= 0.01;
+		contactForm.style.opacity = opacity;
+		if (opacity <= 0) {
+			contactForm.style.display = "none";
+			clearInterval(interval);
+		}
 
 
-    }
-        envelope.addEventListener('click', function (){
-            contactForm.classList.add('show');
-            interval = setInterval(fadeIn, 10);
-            main.classList.add('opacity-toggle');
+	}
+	envelope.addEventListener('click', function () {
+		contactForm.classList.add('show');
+		interval = setInterval(fadeIn, 10);
+		main.classList.add('opacity-toggle');
 
-        });
-        exit.addEventListener('click', function (){
-            interval = setInterval(fadeOut, 10);
-            main.classList.remove('opacity-toggle');
-        });
+	});
+	exit.addEventListener('click', function () {
+		interval = setInterval(fadeOut, 10);
+		main.classList.remove('opacity-toggle');
+	});
 
-        burger.addEventListener('click', function() {
+	burger.addEventListener('click', function () {
 
-            menu.classList.toggle('show-burger');
+		menu.classList.toggle('show-burger');
 
-        });
+	});
 
 };
