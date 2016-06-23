e6f421c20cad6b2076cfabc4865b9a223a8e0437
Patrick Tłuszcz <patricktluszcz@gmail.com>
2016-06-23 22:10:34 +0200

    easy

---
+++ eloquentJS/6-3.js
@@ -0,0 +1,52 @@
+function ArraySeq(array) {
+	this.array = array;
+	this.index = 0;
+}
+
+ArraySeq.prototype.current = function () {
+	return this.array[this.index];
+};
+
+
+
+ArraySeq.prototype.hasNext = function () {
+	return this.array.length >= this.index;
+};
+
+ArraySeq.prototype.next = function () {
+	this.index += 1;
+};
+
+function RangeSeq(froms, to) {
+	this.from = froms;
+	this.to = to;
+	this.currentItem = this.from;
+}
+
+RangeSeq.prototype.next = function () {
+	this.from += 1;
+}
+
+RangeSeq.prototype.current = function () {
+	return this.from;
+}
+
+RangeSeq.prototype.hasNext = function () {
+	return this.from <= this.to;
+}
+
+
+function logFive(sequence) {
+	for (var i = 0; i < 5; i++) {
+
+		if (sequence.hasNext()) {
+			console.log(sequence.current());
+		}
+		sequence.next();
+	}
+
+}
+
+
+console.log(logFive(new ArraySeq([1, 2, 3, 4, 5])));
+console.log(logFive(new RangeSeq(100, 104)));
